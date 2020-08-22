aa9b4d1973ab1dbf0831293a8e797e78aff79f05
sreetejarusum <sreetejarusum@gmailcom>
2020-08-23 00:36:54 +0530

    Program to verify whether it is perfect square or not using binary search

---
+++ Teja2.0/src/math/PerfectSquare.java
@@ -0,0 +1,34 @@
+package math;
+
+public class PerfectSquare {
+	
+	public static boolean isPerfectSquare(int num) {
+		
+		if(num<2) {
+			return true;
+		}
+		
+		int start=2;
+		int end=num/2;
+		
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			
+			if(mid*mid==num) {
+				return true;
+			}else if(mid*mid>num){
+				end=mid-1;
+			}else {
+				start=mid+1;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isPerfectSquare(25));
+		
+	}
+
+}
