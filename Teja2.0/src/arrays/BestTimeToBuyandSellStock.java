e907e8582185b747152f419fa47e26374d450e8e
sreetejarusum <sreetejarusum@gmailcom>
2020-08-23 13:25:33 +0530

    Find max profit by buying and selling stock only once.

--- Teja2.0/src/arrays/BestTimeToBuyandSellStock.java
+++ Teja2.0/src/arrays/BestTimeToBuyandSellStock.java
@@ -1,7 +1,11 @@
 package arrays;
 
 public class BestTimeToBuyandSellStock {
-	
+	/*
+	 * Given an array having stock price on day i, Find the the max profit which 
+	 * can be made , provided the buying and selling is allowed only once, one can not sell
+	 * stock without buying.
+	 */
 	public static int maxProfit(int [] arr) {
 		int maxProfit=0;
 		int min=Integer.MAX_VALUE;
