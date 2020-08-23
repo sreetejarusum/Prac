fc4e34891745b94c14c4ce1cc529dbc23c24f649
sreetejarusum <sreetejarusum@gmailcom>
2020-08-23 13:29:16 +0530

    Added comment in code

--- Teja2.0/src/arrays/BestTimeToBuyandSellStock.java
+++ Teja2.0/src/arrays/BestTimeToBuyandSellStock.java
@@ -5,6 +5,7 @@ public class BestTimeToBuyandSellStock {
 	 * Given an array having stock price on day i, Find the the max profit which 
 	 * can be made , provided the buying and selling is allowed only once, one can not sell
 	 * stock without buying.
+	 * 
 	 */
 	public static int maxProfit(int [] arr) {
 		int maxProfit=0;
