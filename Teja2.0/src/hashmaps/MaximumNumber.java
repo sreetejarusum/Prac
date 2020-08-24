0ac5fb4250368934c009fd8f8c49ba6318cec9ee
sreetejarusum <sreetejarusum@gmailcom>
2020-08-24 23:24:06 +0530

    If the number is appeared more than array.lenth/2 times than it is maximum number.

---
+++ Teja2.0/src/hashmaps/MaximumNumber.java
@@ -0,0 +1,39 @@
+package hashmaps;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MaximumNumber {
+	
+	public static int getMaximumNumber(int [] arr) {
+		HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+		for (Integer in:arr) {
+			
+			if(!hm.containsKey(in)) {
+				hm.put(in, 1);
+			}else {
+				hm.put(in, hm.get(in)+1);
+			}
+		}
+		Set<Entry<Integer,Integer>> entry=hm.entrySet();
+		for(Entry<Integer, Integer> i:entry) {
+			if(i.getValue()>arr.length/2) {
+				return i.getKey();
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+//		int [] arr= {2,2,1,3,3,2,3,2};
+//		int [] arr= {2,2,1,3,3,2,3,2,3,2,2};
+//		int [] arr= {2};
+		int [] arr= {1,1,2};
+		System.out.println(getMaximumNumber(arr));
+		
+		
+	}
+
+}
