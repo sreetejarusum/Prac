92a87e6241cbacee7455d577ba6c223069a7065d
sreetejarusum <sreetejarusum@gmailcom>
2020-08-26 20:13:52 +0530

    Program to move zeros to the end of array

--- Teja2.0/src/arrays/MoveZerosToEnd.java
+++ Teja2.0/src/arrays/MoveZerosToEnd.java
@@ -2,10 +2,29 @@ package arrays;
 
 public class MoveZerosToEnd {
 	
-//	pub
+	public static int[] moveZerosToEnd(int [] nums) {
+		int i=0;
+		for(int j=0;j<nums.length;j++) {
+			
+			if(nums[j]!=0) {
+				nums[i++]=nums[j];
+			}
+		}
+		
+		while(i<nums.length) {
+			nums[i++]=0;
+		}
+		
+		return nums;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(1/2);
+		int [] arr={0,4,5,0,3,0,-1};//4,5,3,-1,0,0,0
+		moveZerosToEnd(arr);
+		
+		for(Integer in:arr) {
+			System.out.print(in+" ");
+		}
 	}
 
 }
